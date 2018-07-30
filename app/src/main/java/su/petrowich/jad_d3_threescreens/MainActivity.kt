434b8b21514932178eed7e8297d8098c6966173e
Petr Kreslin <petr.kreslin@gmail.com>
2018-07-30 15:05:43 +0300

    Initial commit

---
+++ app/src/main/java/su/petrowich/jad_d3_threescreens/MainActivity.kt
@@ -0,0 +1,12 @@
+package su.petrowich.jad_d3_threescreens
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
